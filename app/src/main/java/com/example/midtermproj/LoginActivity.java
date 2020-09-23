0ee9617f93526d67b65d716aab82554f44d2d886
Lenomi <minhlnbeen@gmail.com>
2020-09-23 17:52:57 +0700

    update login layout and login activity

--- app/src/main/java/com/example/midtermproj/LoginActivity.java
+++ app/src/main/java/com/example/midtermproj/LoginActivity.java
@@ -5,16 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textInputUsername = findViewById(R.id.username_input);
+        textInputPassword= findViewById(R.id.password_input);
     }
 
+    private boolean validateEmail() {
+        String emailInput = textInputUsername.getEditText().getText().toString().trim();
+        if (emailInput.isEmpty())
+        {
+            textInputUsername.setError("Field cannot be empty");
+            return false;
+        } else {
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        if (passwordInput.isEmpty())
+        {
+            textInputPassword.setError("Field cannot be empty");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+
     public void onClickLogin(View view) {
+        if(!validateEmail() | !validatePassword()) {
+            return;
+        }
+        String input = "Email: " + textInputUsername.getEditText().getText().toString();
+        input+="\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this,input, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         this.startActivity(intent);
     }
